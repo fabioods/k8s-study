2ba0110dcfb1b38a01b94ea12df15b84c3da148c
Fábio dos santos <fah_ds@live.com>
2025-04-08 17:16:47 -0300

    Use volumes

--- server.go
+++ server.go
@@ -13,5 +13,14 @@ func main() {
 		msg := fmt.Sprintf("Hello, %s! You are %s years old.", name, age)
 		w.Write([]byte(msg))
 	})
+	http.HandleFunc("/myfamily", func(w http.ResponseWriter, r *http.Request) {
+		data, err := os.ReadFile("family/myfamily.txt")
+		if err != nil {
+			http.Error(w, "File not found", http.StatusNotFound)
+			return
+		}
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write(data)
+	})
 	http.ListenAndServe(":8080", nil)
 }
