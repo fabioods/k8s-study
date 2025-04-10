c72b36c6263e75e61cafdcc682336bbdfddfa092
Fábio dos santos <fah_ds@live.com>
2025-04-10 14:00:32 -0300

    Add metric server config

--- server.go
+++ server.go
@@ -34,10 +34,10 @@ func main() {
 	})
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		duration := time.Since(startedAt)
-		if duration.Seconds() > 25 {
+		if duration.Seconds() < 10 || duration.Seconds() > 60 {
 			w.Header().Set("Content-Type", "text/plain")
 			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(fmt.Sprintf("Server has been running for %s, which is too long.", duration)))
+			w.Write([]byte(fmt.Sprintf("Server has been running for %s, which is too short.", duration)))
 			return
 		}
 	})
